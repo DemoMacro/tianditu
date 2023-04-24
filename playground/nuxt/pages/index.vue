e607337f585bc0a6704d8f40c8eecd4703dc0c6e
Demo Macro <abc@imst.xyz>
2023-04-24 11:18:54 +0800

    chore: move vue files to pages dir

---
+++ playground/nuxt/pages/index.vue
@@ -0,0 +1,41 @@
+<script setup>
+const config = useRuntimeConfig();
+
+const center = ref([0, 0]);
+const zoom = ref(18);
+
+navigator.geolocation.getCurrentPosition(
+  (position) => {
+    center.value = [position.coords.longitude, position.coords.latitude];
+  },
+  null,
+  {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0,
+  }
+);
+
+useHead({
+  script: [
+    {
+      src: `https://api.tianditu.gov.cn/api?v=4.0&tk=${config.public.tianditu.browserKey}`,
+    },
+  ],
+});
+</script>
+
+<template>
+  <div class="h-screen">
+    <tdt-map
+      :tk="config.public.tianditu.browserKey"
+      :center="center"
+      :zoom="zoom"
+      class="block"
+      style="width: 100%; height: 100%"
+    >
+      <tdt-control-zoom />
+      <tdt-control-scale />
+    </tdt-map>
+  </div>
+</template>
